bcf83faa7a6fc66af1831c951e907fc3d6949ad3
Maxwell Born <borncmaxwell@gmail.com>
2024-10-19 17:06:24 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Modify customerName to uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Set best customer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Overwrite best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Attempt to change leastFavoriteCustomer (will fail)
+const leastFavoriteCustomer = 'someone else';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob'; // This will throw an error
+}
